ee5b638afb7c45bdaac3e10781702601142c9988
prakashkhadka7524 <prakashkhadka7524@outlook.com>
2023-09-16 11:55:08 -0700

    first commit

---
+++ parakash/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.util.Objects;
+
+public class Main {
+    public static void main(String[] args) {
+        Project
+        project.setDifficulty(Objects.isNull(difficulty)
+                ? curProject.getDifficulty(): difficulty);
+    }
+}
